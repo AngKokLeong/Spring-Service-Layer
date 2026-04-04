a606a98014530f40c807cf9f458357a28e7c3797
Ang Kok Leong <ang.kok.leong.work@gmail.com>
2026-04-04 03:18:27 +0000

    SSL-007 added methods in LoanRecordRepository

--- src/main/java/com/workshop/bookshelf/repository/LoanRecordRepository.java
+++ src/main/java/com/workshop/bookshelf/repository/LoanRecordRepository.java
@@ -1,11 +1,18 @@
 package com.workshop.bookshelf.repository;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import com.workshop.bookshelf.model.Book;
 import com.workshop.bookshelf.model.LoanRecord;
 
 @Repository
 public interface LoanRecordRepository extends JpaRepository<LoanRecord, Long>{
     
+    List<LoanRecord> findByReturnedFalse();
+    Boolean existsByBookAndReturnedFalse(Book book);
+
+
 }
